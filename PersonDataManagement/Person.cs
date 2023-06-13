b001243bb8a8529aab299bdcaa93b3a92be63f9f
“karandepale” <karandepale111@gmail.com>
2023-06-13 11:51:22 +0530

    Add: Two person name whose age is less then 60 year

--- PersonDataManagement/Person.cs
+++ PersonDataManagement/Person.cs
@@ -12,5 +12,14 @@ namespace PersonDataManagement
         public string Name { get; set; }
         public string Address { get; set; }
         public int Age { get; set; }
+
+        public List<Person> RetrieveTop2(List<Person> personList)
+        {
+            var filteredPersons = personList.Where(person => person.Age < 60)
+                                            .Take(2)
+                                            .ToList();
+
+            return filteredPersons;
+        }
     }
 }
--- PersonDataManagement/Program.cs
+++ PersonDataManagement/Program.cs
@@ -13,12 +13,12 @@ namespace PersonDataManagement
             List<Person> personList = new List<Person>
             {
                 new Person { SSN = "1", Name = "Karan", Address = "123 Main St", Age = 30 },
-                new Person { SSN = "2", Name = "Manoj", Address = "456 Elm St", Age = 25 },
-                new Person { SSN = "3", Name = "Khushi", Address = "789 Oak St", Age = 40 },
-                new Person { SSN = "4", Name = "Krishna", Address = "789 Oak St", Age = 65 },
-                new Person { SSN = "5", Name = "Ajay", Address = "789 Oak St", Age = 70 },
-                new Person { SSN = "6", Name = "Mahi", Address = "789 Oak St", Age = 80 },
-                new Person { SSN = "7", Name = "Atul", Address = "789 Oak St", Age = 40 }
+                new Person { SSN = "2", Name = "Manoj", Address = "456 Elm Sm", Age = 25 },
+                new Person { SSN = "3", Name = "Khushi", Address = "789 Oak Sh", Age = 40 },
+                new Person { SSN = "4", Name = "Krishna", Address = "789 Oak Sf", Age = 65 },
+                new Person { SSN = "5", Name = "Ajay", Address = "789 Oak Sd", Age = 70 },
+                new Person { SSN = "6", Name = "Mahi", Address = "789 Oak Sq", Age = 90 },
+                new Person { SSN = "7", Name = "Atul", Address = "789 Oak Sr", Age = 40 }
             };
 
             foreach (var personn in personList)
@@ -26,6 +26,19 @@ namespace PersonDataManagement
                 Console.WriteLine("SSN: " + personn.SSN);
                 Console.WriteLine("Name: " + personn.Name);
                 Console.WriteLine("Address: " + personn.Address);
+                Console.WriteLine("Age: " + personn.Age);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(" two Person name whose age is less then 60 year ");
+
+            Person person = new Person();
+            List<Person> filteredPersons = person.RetrieveTop2(personList);
+
+            // Print the filtered persons
+            foreach (var personn in filteredPersons)
+            {
+                Console.WriteLine("Name: " + personn.Name);
                 Console.WriteLine("Age: " + personn.Age);
                 Console.WriteLine();
             }
